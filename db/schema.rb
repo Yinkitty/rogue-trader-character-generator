4b24141a0749f81666b666d8a7fcb1cba7598747
Kryptykfysh <Kryptykfysh@localhost>
2010-07-19 07:55:08 +0100

    After default Birthrights db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100719064626) do
+ActiveRecord::Schema.define(:version => 20100719065245) do
 
   create_table "birthrights", :force => true do |t|
     t.string   "name"
