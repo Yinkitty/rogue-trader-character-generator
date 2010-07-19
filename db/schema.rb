965077f3d418bb714473f0c2c1cd2960234d9553
Kryptykfysh <Kryptykfysh@localhost>
2010-07-19 07:51:23 +0100

    After default Home Worlds db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100717081246) do
+ActiveRecord::Schema.define(:version => 20100719064626) do
 
   create_table "birthrights", :force => true do |t|
     t.string   "name"
